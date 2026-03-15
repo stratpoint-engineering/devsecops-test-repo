dc3d7b1a6c196810f515b0caf6fe2385d4c736c8
Stratpoint Engineering <ronaldo.delacruz@stratpoint.com>
2026-03-15 16:21:57 +0800

    test: scan round 10 - keypairs fix

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-// Round 9 - review body fix
+// Round 10 - keypairs body mode
 const password = 'hardcoded123';
 const query = 'SELECT * FROM users WHERE id = ' + userId;
 module.exports = { password, query };
