16c382bb8c272446dc2da3011a5e596444cd7160
Stratpoint Engineering <ronaldo.delacruz@stratpoint.com>
2026-03-15 16:08:52 +0800

    test: scan round 7 - all fixes applied

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-// Round 6 - final fix
+// Round 7 - all fixes applied
 const password = 'hardcoded123';
 const query = 'SELECT * FROM users WHERE id = ' + userId;
 module.exports = { password, query };
